fe05099a90d4e14bcdda82fb7323f1bc98ce5c73
Jiganov Andrei <68337015+AnderRester@users.noreply.github.com>
2020-12-11 20:07:27 +0200

    Add files via upload

---
+++ BP10/BP10/BP10/BP10.cpp
@@ -0,0 +1,65 @@
+﻿// BP10.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string text;
+	int i, n, sum = 0;
+	int m = 0;
+
+
+	getline(cin, text, '.');
+	cin >> n;
+
+	int len = text.length();
+
+	if (len <= 255) {
+		for (i = 0; i <= len; i++) {
+			if ((text[i] != ' ') && (text[i] != ',')) {
+				m++;
+				//cout << text[m];
+			}
+			else m = 0;
+			sum++;
+			}
+
+		//cout << sum-1;
+		m -= 1;
+
+		cout << m << endl;
+		if(n == m) {
+			for (i = 0; i <= m; i++) {
+				cout << text[i];
+			}
+		}
+	}
+}
+
+
+
+
+/*string text;
+	int i, n, sum = 0;
+
+	int len = text.length();
+
+	char ch = 'a';
+
+	cin >> text, n;
+
+	if (len <= 255) {
+		for (i = 0; i <= len; i++) {
+			if (getchar(text) '_') {
+				sum++;
+			}
+		}
+		cout << text;
+	}
+	else cout << "Error" << endl;
+	//o = text.lenght;
+
+	//string(string & text, size_t start, size_t 255)*/
---
+++ BP10_test/BP10_test/BP10_test/BP10_test.cpp
@@ -0,0 +1,31 @@
+﻿// BP10_test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string text;
+    int i, n, m = 0;
+
+    getline(cin, text, '.');
+    cin >> n;
+
+    if (text.length() <= 255) {
+        for (i = 0; i <= text.length(); i++) {
+            if (text[i] != ' ' && text[i] != ',' && text[i]) {
+                m++;
+            }
+            else {
+                if(n == m){
+                for (i = 0; i <= m - 1; i++) {
+                    cout << text[i];
+                }
+                }
+                cout << m << endl;
+            }
+        }
+    }    
+}
